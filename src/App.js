67f0c001f646fe5ebdb820d6a9c11a33c5b02034
Cole Bemis <colebemis@gmail.com>
2017-11-20 22:57:58 -0800

    Import feather-icons

--- src/App.js
+++ src/App.js
@@ -1,20 +1,9 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { icons } from 'feather-icons';
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+    return <div>{Object.keys(icons).map(icon => <div>{icon}</div>)}</div>;
   }
 }
 
