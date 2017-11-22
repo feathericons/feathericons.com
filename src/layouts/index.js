9f664e27ac753731be7dff3f75a01099dadc5a3a
Cole Bemis <colebemis@gmail.com>
2017-11-22 15:39:58 -0800

    Add base styles

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
 
+import './index.css';
+
 import Header from '../components/Header';
 
 const propTypes = {
