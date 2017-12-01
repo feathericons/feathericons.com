ee0225015b714517b158c13d3a97c6852a0ca6e3
Cole Bemis <colebemis@gmail.com>
2017-12-01 15:34:44 -0800

    Update boxShadows

--- src/theme.js
+++ src/theme.js
@@ -36,8 +36,8 @@ export default {
   },
   shadows: [
     'none',
-    '0 0 0 1px rgba(0,0,0,0.05), 0 2px 8px rgba(0,0,0,0.1)',
-    '0 0 0 1px rgba(0,0,0,0.05), 0 4px 16px rgba(0,0,0,0.1)',
+    '0 0 0 1px rgba(0, 0, 0, 0.1), 0 2px 8px rgba(0, 0, 0, 0.1)',
+    '0 0 0 1px rgba(0, 0, 0, 0.1), 0 4px 16px rgba(0, 0, 0, 0.1)',
   ],
   radii: [0, 2, 4, 8],
 };
