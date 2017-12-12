cb495d96f4f445fb32b0a3b6867c128be40a70d4
Cole Bemis <colebemis@gmail.com>
2017-12-11 22:22:25 -0800

    Style 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,5 +1,13 @@
 import React from 'react';
+import styled from 'styled-components';
 import Helmet from 'react-helmet';
+import { Heading, Text as BaseText } from 'rebass';
+
+import Container from '../components/Container';
+
+const Text = styled(BaseText)`
+  line-height: 1.5;
+`;
 
 function NotFoundPage() {
   return (
@@ -7,7 +15,14 @@ function NotFoundPage() {
       <Helmet>
         <title>Page Not Found | Feather</title>
       </Helmet>
-      <h1>Page Not Found</h1>
+      <Container py={7}>
+        <Heading is="h1" mb={4} f={6} color="gray9" center>
+          Oops.
+        </Heading>
+        <Text f={4} color="gray8" center>
+          We couldn&apos;t find what you were looking for.
+        </Text>
+      </Container>
     </div>
   );
 }
