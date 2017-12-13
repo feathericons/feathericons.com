4f0ff00de7151476913d9992a3529dff6e5ef485
Cole Bemis <colebemis@gmail.com>
2017-12-12 17:24:32 -0800

    Add meta tags

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -50,7 +50,14 @@ function Template({
       <div>
         <Helmet>
           <title>{title}</title>
+
           <meta name="description" content={description} />
+
+          <meta property="og:title" content={title} />
+          <meta property="og:description" content={description} />
+
+          <meta name="twitter:card" content="summary" />
+          <meta name="twitter:creator" content="@colebemis" />
         </Helmet>
         <Header />
         {children()}
