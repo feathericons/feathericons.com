3b941f5e0ab6b411a68b88e0948c3df9782cae6f
Cole Bemis <colebemis@gmail.com>
2017-12-12 19:16:07 -0800

    Add favicon pngs

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -59,6 +59,19 @@ function Template({
 
           <meta name="twitter:card" content="summary" />
           <meta name="twitter:creator" content="@colebemis" />
+
+          <link
+            rel="icon"
+            type="image/png"
+            href="favicon-32x32.png"
+            sizes="32x32"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            href="favicon-16x16.png"
+            sizes="16x16"
+          />
         </Helmet>
         <Header />
         {children()}
