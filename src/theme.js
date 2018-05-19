966f9bad9e8f3edef7bcb80207917565735e14b8
Cole Bemis <colebemis@gmail.com>
2018-05-18 22:16:10 -0700

    Update theme

--- src/theme.js
+++ src/theme.js
@@ -1,49 +1,43 @@
-import palx from 'palx'
+const space = [0, 4, 8, 12, 16, 24, 32, 64, 128, 256, 512]
 
-export const space = [0, 4, 8, 12, 16, 24, 32, 64]
-
-export const font =
+const font =
   '-apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"'
 
-export const weights = [400, 500]
+const weights = [400, 500]
 
-export const lineHeights = {
+const lineHeights = {
   none: 1,
   tight: 1.25,
   normal: 1.5,
   loose: 2,
 }
 
-const palette = palx('#06f')
-
-const flattened = Object.keys(palette).reduce((accumlator, key) => {
-  const value = palette[key]
-
-  if (Array.isArray(value)) {
-    accumlator[key] = value[5]
-    value.forEach((element, i) => {
-      accumlator[key + i] = element
-    })
-  } else {
-    accumlator[key] = value
-  }
-
-  return accumlator
-}, {})
-
-export const colors = {
+const colors = {
+  base: '#06f',
+  black: '#000',
   white: '#fff',
-  ...flattened,
+
+  // Grays generated with palx (https://palx.jxnblk.com/) using a base of #06f
+  gray0: '#f8f9fa',
+  gray1: '#ebedf0',
+  gray2: '#dee0e5',
+  gray3: '#cfd3d9',
+  gray4: '#bec4cc',
+  gray5: '#acb3be',
+  gray6: '#97a0ae',
+  gray7: '#7f899a',
+  gray8: '#626c7d',
+  gray9: '#393f49',
 }
 
-export const shadows = {
+const shadows = {
   none: 'none',
   small: '0 0 0 1px rgba(0, 0, 0, 0.1), 0 2px 4px rgba(0, 0, 0, 0.1)',
   medium: '0 0 0 1px rgba(0, 0, 0, 0.1), 0 4px 16px rgba(0, 0, 0, 0.1)',
   outline: `0 0 0 3px ${colors.base}`,
 }
 
-export const radius = 4
+const radius = 4
 
 export default {
   space,
