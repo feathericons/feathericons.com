547a33dc3e9b43fab1d21901a53bb2bb57336e7d
Cole Bemis <colebemis@gmail.com>
2018-05-28 20:53:15 -0700

    Update NoResults

--- src/components/NoResults.js
+++ src/components/NoResults.js
@@ -1,21 +1,8 @@
-import React from 'react'
 import { string } from 'prop-types'
+import React from 'react'
 import styled from 'styled-components'
 import Flex from './Flex'
-import { radius } from '../theme'
 import Text from './Text'
-
-const propTypes = {
-  value: string,
-}
-
-const defaultProps = {
-  value: '',
-}
-
-const Rounded = styled(Flex)`
-  border-radius: ${radius}px;
-`
 
 const BreakWord = styled(Text)`
   word-break: break-word;
@@ -23,15 +10,27 @@ const BreakWord = styled(Text)`
 
 function NoResults({ value }) {
   return (
-    <Rounded direction="column" align="center" bg="gray0" px={6} py={7}>
+    <Flex
+      direction="column"
+      align="center"
+      px={6}
+      py={7}
+      bg="gray0"
+      borderRadius={1}
+    >
       <BreakWord f={3} lineHeight="normal" color="gray8" center>
-        No results found {value && `for "${value}"`}
+        No results found for &ldquo;{value}&rdquo;
       </BreakWord>
-    </Rounded>
+    </Flex>
   )
 }
 
-NoResults.propTypes = propTypes
-NoResults.defaultProps = defaultProps
+NoResults.propTypes = {
+  value: string,
+}
+
+NoResults.defaultProps = {
+  value: '',
+}
 
 export default NoResults
