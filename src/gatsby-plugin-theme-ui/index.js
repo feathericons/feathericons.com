3d502c7b19c056cb7dbd3662c1dde2f6360248bb
Cole Bemis <colebemis@github.com>
2019-11-07 00:27:11 -0800

    Use pixel values

--- src/gatsby-plugin-theme-ui/index.js
+++ src/gatsby-plugin-theme-ui/index.js
@@ -1,33 +1,12 @@
 export default {
-  space: [
-    '0',
-    '0.25rem',
-    '0.5rem',
-    '0.75rem',
-    '1rem',
-    '1.5rem',
-    '2rem',
-    '2.5rem',
-    '3rem',
-    '4rem',
-    '5rem',
-    '6rem',
-  ],
+  space: [0, 4, 8, 12, 16, 24, 32, 40, 48, 64, 80, 96],
   fonts: {
     sansSerif:
       '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"',
     monospace:
       'SFMono-Regular, Consolas, "Liberation Mono", Menlo, Courier, monospace',
   },
-  fontSizes: [
-    '0.75rem',
-    '0.875rem',
-    '1rem',
-    '1.125rem',
-    '1.25rem',
-    '1.5rem',
-    '2rem',
-  ],
+  fontSizes: [12, 14, 16, 18, 20, 24, 32],
   fontWeights: {
     normal: 400,
     medium: 500,
@@ -57,7 +36,7 @@ export default {
       '#212529',
     ],
   },
-  radii: ['0', '4px'],
+  radii: [0, 4],
   shadows: [
     'none',
     '0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06)',
