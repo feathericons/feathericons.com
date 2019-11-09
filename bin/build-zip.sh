1d7364a61e07a9b004c3bb2baaa7e2dc1d961255
Cole Bemis <colebemis@github.com>
2019-11-08 21:42:23 -0800

    Generate zip dynamically

--- bin/build-zip.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-# compress 'icons' directory into feather.zip
-zip -r -j static/feather.zip ./node_modules/feather-icons/dist/icons/
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,4 +1,7 @@
 /** @jsx jsx */
+import download from 'downloadjs'
+import { icons } from 'feather-icons'
+import JSZip from 'jszip'
 import { jsx } from 'theme-ui'
 import logDownload from '../utils/logDownload'
 import logOutboundLink from '../utils/logOutboundLink'
@@ -50,10 +53,11 @@ function Hero() {
           Get started
         </Button>
         <Button
-          as={OutboundLink}
-          href="/feather.zip"
-          onClick={() => logDownload('all')}
-          download
+          onClick={async () => {
+            const zip = await generateZip()
+            download(zip, 'feather.zip')
+            logDownload('all')
+          }}
           sx={{ variant: 'buttons.outline' }}
         >
           Download all
@@ -63,6 +67,14 @@ function Hero() {
       <CarbonAd sx={{ marginTop: 8 }} />
     </div>
   )
+}
+
+function generateZip(options) {
+  const zip = new JSZip()
+  Object.values(icons).forEach(icon =>
+    zip.file(`${icon.name}.svg`, icon.toSvg(options)),
+  )
+  return zip.generateAsync({ type: 'blob' })
 }
 
 export default Hero
